0dd4a798e5fa3ff8a818093bfd89d0196cc60478
Abdulhakeem Muhammed <dev.abdulhakeem@gmail.com>
2024-07-22 10:07:27 +0100

    Debug

--- client/src/pages/dashboard/IPR/index.jsx
+++ client/src/pages/dashboard/IPR/index.jsx
@@ -3,29 +3,34 @@ import { useLocation } from 'react-router-dom';
 
 const IPR = () => {
   const { state } = useLocation();
-  const { ipr } = state;
   const hash = window.location.pathname.split("/").pop();
 
-  console.log({hash, ipr});
+  if (!state || !state.ipr) {
+    return <div>Loading...</div>; // Or any other loading/error message
+  }
 
-  const ProdIpr = ({productIpr, index}) => (
-     <p style={{marginLeft: "10px"}}>Product {index+1} IPR: {productIpr}</p>
+  const { ipr } = state;
+
+  console.log({ hash, ipr });
+
+  const ProdIpr = ({ productIpr, index }) => (
+    <p style={{ marginLeft: "10px" }}>Product {index + 1} IPR: {productIpr}</p>
   );
 
   return (
-    <div style={{overflow: "scroll"}}>
-      <h3 style={{fontWeight: "bold"}}>Product Code: {hash}</h3>
+    <div style={{ overflow: "scroll" }}>
+      <h3 style={{ fontWeight: "bold" }}>Product Code: {hash}</h3>
       <br />
       <div>
-        <p style={{marginBottom: "20px"}}>Packages & Products Internationalized Packages Records:</p>
+        <p style={{ marginBottom: "20px" }}>Packages & Products Internationalized Packages Records:</p>
         <hr />
         {
-          ipr.map((pkg, index) => (
-            <div key={pkg.packageIpr} style={{marginBottom: "2rem"}}>
-              <p style={{fontWeight: "bold"}}>Package {index+1} IPR: {pkg.packageIpr}</p>
+          ipr?.map((pkg, index) => (
+            <div key={pkg.packageIpr} style={{ marginBottom: "2rem" }}>
+              <p style={{ fontWeight: "bold" }}>Package {index + 1} IPR: {pkg.packageIpr}</p>
               {
-                pkg.productIpr.map((prdIpr, index) => (
-                  <ProdIpr productIpr={prdIpr} index={index} key={index} />
+                pkg.productIpr.map((prdIpr, idx) => (
+                  <ProdIpr productIpr={prdIpr} index={idx} key={idx} />
                 ))
               }
               <hr />
